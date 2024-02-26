7f9ee33cc49875e497c02e62285e5fbb2cbe5ffa
Kyle Mathews <mathews.kyle@gmail.com>
2024-02-26 14:06:37 -0800

    Refactor <RecipeCard /> to its own file so can be reused

---
+++ packages/app/src/components/recipe-card.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom"
+import {
+  Flex,
+  Heading,
+  Link as RadixLink,
+  Text,
+  Separator,
+} from "@radix-ui/themes"
+import TimeAgo from "javascript-time-ago"
+
+// English.
+import en from "javascript-time-ago/locale/en"
+
+TimeAgo.addDefaultLocale(en)
+
+// Create formatter (English).
+const timeAgo = new TimeAgo(`en-US`)
+
+export default function RecipeCard({ recipe }) {
+  return (
+    <Flex direction="column" gap="3">
+      <Heading size="3" weight="medium">
+        <Link
+          to={`/recipes/${recipe.id}`}
+          style={{
+            color: `inherit`,
+            textDecoration: `none`,
+          }}
+        >
+          {recipe.name}
+        </Link>
+      </Heading>
+      <Text color="gray" size="2">
+        Added {timeAgo.format(recipe.created_at)}
+      </Text>
+    </Flex>
+  )
+}
