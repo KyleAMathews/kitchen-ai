44e7a5bfb01d679189195574ef3982ba6ea96df0
Kyle Mathews <mathews.kyle@gmail.com>
2025-08-30 15:18:07 -0600

    Just show nothing for ingredients when not searching

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -66,15 +66,15 @@ function Dashboard() {
     (q) =>
       searchQuery.length > 0
         ? q
-            .from({ recipesCollection })
-            .where(({ recipesCollection }) =>
-              or(
-                ilike(recipesCollection.name, `%${searchQuery}%`),
-                ilike(recipesCollection.description, `%${searchQuery}%`)
-              )
+          .from({ recipesCollection })
+          .where(({ recipesCollection }) =>
+            or(
+              ilike(recipesCollection.name, `%${searchQuery}%`),
+              ilike(recipesCollection.description, `%${searchQuery}%`)
             )
+          )
         : // .limit(3)
-          q.from({ recipesCollection }),
+        q.from({ recipesCollection }),
     [searchQuery]
   )
 
@@ -82,12 +82,12 @@ function Dashboard() {
     (q) =>
       searchQuery.length > 0
         ? q
-            .from({ ingredientsCollection })
-            .where(({ ingredientsCollection }) =>
-              ilike(ingredientsCollection.name, `%${searchQuery}%`)
-            )
+          .from({ ingredientsCollection })
+          .where(({ ingredientsCollection }) =>
+            ilike(ingredientsCollection.name, `%${searchQuery}%`)
+          )
         : // .limit(3)
-          q.from({ ingredientsCollection }),
+        q.from({ ingredientsCollection }),
     [searchQuery]
   )
 
@@ -203,7 +203,7 @@ function Dashboard() {
               </>
             ) : (
               <Text color="gray">
-                {isSearching ? `No results` : `Add ingredients to get started!`}
+                {isSearching ? `No results` : ``}
               </Text>
             )}
           </Flex>
